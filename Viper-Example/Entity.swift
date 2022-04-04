8e1998fbb467bac76cb4f48ed447106643744430
sefaceliik <62655168+sefaceliik@users.noreply.github.com>
2022-04-04 14:40:47 +0300

    Project created.

---
+++ Viper-Example/Entity.swift
@@ -0,0 +1,15 @@
+//
+//  Entity.swift
+//  Viper-Example
+//
+//  Created by Sefa Çelik on 4.04.2022.
+//
+
+import Foundation
+
+// Struct
+
+struct Crypto : Decodable {
+    let currency : String
+    let price : String
+}
---
+++ Viper-Example/Router.swift
@@ -0,0 +1,45 @@
+//
+//  Router.swift
+//  Viper-Example
+//
+//  Created by Sefa Çelik on 4.04.2022.
+//
+
+import Foundation
+import UIKit
+
+// Class
+// We setup every component here to orchestrate and route the application
+
+typealias EntryPoint = AnyView & UIViewController
+
+protocol AnyRouter {
+    var entry : EntryPoint? {get}
+    static func startExecution() -> AnyRouter
+}
+
+class CryptoRouter : AnyRouter {
+    var entry: EntryPoint?
+    
+    static func startExecution() -> AnyRouter {
+        
+        let router = CryptoRouter()
+        
+        var view : AnyView = CryptoViewController()
+        var presenter : AnyPresenter = CryptoPresenter()
+        var interactor : AnyInteractor = CryptoInteractor()
+        
+        view.presenter = presenter
+        
+        presenter.view = view
+        presenter.router = router
+        presenter.interactor = interactor
+        
+        interactor.presenter = presenter
+        
+        router.entry = view as? EntryPoint
+        
+        return router
+        
+    }
+}
